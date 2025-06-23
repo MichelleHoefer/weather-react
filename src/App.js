f88e064563126004eecf5dd9fe02cd486d6f0d66
Michelle Hoefer <michellehoefer7@gmail.com>
2025-06-23 16:56:59 +0100

    added some html to add basics of page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
+import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather />
-      </header>
+      <div className="container  ">
+        <header className="App-header">React Weather App</header>
+        <main className="weather">
+          <form>
+            <input type="search" placeholder="Enter a city..." />
+            <input type="submit" value="Search" />
+          </form>
+          <h1>Tokyo</h1>
+          <ul>
+            <li>Monday 16:28</li>
+            <li>Mostly Cloudy</li>
+          </ul>
+          <div className="row">
+            <div className="col-6">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="weather-icon"
+              ></img>
+              6°C
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Humidity: %</li>
+                <li>Description: </li>
+                <li>Wind: km/h</li>
+              </ul>
+            </div>
+          </div>
+        </main>
+        <footer>
+          This project was created by{" "}
+          <a href="https://www.linkedin.com/in/michelle-hoefer-0b478229b/">
+            Michelle Hoefer
+          </a>
+          , open-sourced on{" "}
+          <a href="https://github.com/MichelleHoefer/weather-react">Github</a>{" "}
+          and hosted on{" "}
+          <a href="https://react-weather-app-hoefer.netlify.app/">Netlify.</a>
+        </footer>
+      </div>
     </div>
   );
 }
