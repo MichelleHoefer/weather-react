8da00a2728c361bbae212b245125780c8837ef4a
Michelle Hoefer <michellehoefer7@gmail.com>
2025-06-23 15:57:35 +0100

    removed unwanted files and css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Weather city="New York" />
+        <Weather />
       </header>
     </div>
   );
